c2a88b017c84ca46c877e8800d1a852ff80c1809
AbdurM <47498415+AbdurM@users.noreply.github.com>
2021-09-11 21:18:45 +1000

    Dynamic location logic

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import yelp from '../api/yelp';
+import * as Location from 'expo-location';
 
 export default () => {
     const [results, setResults] = useState([]);
@@ -7,11 +8,22 @@ export default () => {
 
     const searchApi = async (searchTerm) => {
         try {
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+              setErrorMsg('Permission to access location was denied');
+              return;
+            }
+      
+            let location = await Location.getCurrentPositionAsync({});
+            console.log(location.coords.latitude);
+
             const response = await yelp.get("/search", {
                 params: {
                     limit: 50,
                     term: searchTerm,
-                    location: 'sydney'
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude
                 }
             });
             setResults(response.data.businesses);
